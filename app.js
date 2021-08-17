70dd275fb7bc4420d5518253c43bfe3171269116
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-17 21:18:26 +0900

    feat: add content when click item
    news 를 클릭하면 뉴스 제목을 아래줄에 추가 하는 코드 작성
    계속 append 되기 때문에 화면 전환 코드 추가 필요

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const container = document.getElementById("root");
 
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
@@ -7,11 +10,30 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
+window.addEventListener('hashchange', () => {
+	const id = location.hash.substr(1);
+
+	ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+	ajax.send();
+
+	const newsInfo = JSON.parse(ajax.response);
+	const title = document.createElement('h1');
+	title.innerHTML = newsInfo.title;
+
+	content.appendChild(title);
+})
+
 let li = "";
 for (let i=0; i<10; i++) {
-	const li = document.createElement("li");
-	li.innerHTML = newsFeed[i].title;
+	const li = document.createElement("li"),
+		a = document.createElement("a");
+	
+	a.href = `#${newsFeed[i].id}`;
+	a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+	li.appendChild(a);
 	ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
