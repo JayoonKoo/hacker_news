476998016639c6f1da52a257bd407308a0890dba
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-24 21:16:20 +0900

    refactor: add function getData
    ajax 요청하는 부분을 함수로 만들어서 반복 줄임

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const container = document.getElementById("root");
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+function getData(url) {
+	ajax.open('GET', url, false);
+	ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+	return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
 window.addEventListener('hashchange', () => {
 	const id = location.hash.substr(1);
 
-	ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-	ajax.send();
-
-	const newsInfo = JSON.parse(ajax.response);
+	const newsInfo = getData(CONTENT_URL.replace('@id', id));
 	const title = document.createElement('h1');
+
 	title.innerHTML = newsInfo.title;
 
 	content.appendChild(title);
