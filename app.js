871f3794a3ddbef23e7262dc586ef9721e459482
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-24 21:54:27 +0900

    feat: add router
    해시태그 변함에 따라 라우터 호출되게 구현

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const container = document.getElementById("root");
@@ -11,35 +10,48 @@ function getData(url) {
 	return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+	const newsFeed = getData(NEWS_URL);
+	const newsList = [];
 
-window.addEventListener('hashchange', () => {
-	const id = location.hash.substr(1);
+	newsList.push('<ul>');
+	for (let i=0; i<10; i++) {
+		newsList.push(`
+			<li>
+				<a href="#${newsFeed[i].id}">
+					${newsFeed[i].title} (${newsFeed[i].comments_count})
+				</a>
+			</li>
+		`);
+	}
+	newsList.push('</ul>');
+	container.innerHTML = newsList.join('');
 
-	const newsInfo = getData(CONTENT_URL.replace('@id', id));
-	const title = document.createElement('h1');
-
-	title.innerHTML = newsInfo.title;
-
-	content.appendChild(title);
-})
-
-let li = "";
-for (let i=0; i<10; i++) {
-	const div = document.createElement('dev');
-
-	div.innerHTML =`
-		<li>
-			<a href="#${newsFeed[i].id}">
-				${newsFeed[i].title} (${newsFeed[i].comments_count})
-			</a>
-		</li>
-	`
-	
-	// ul.appendChild(div.children[0]);
-	ul.appendChild(div.firstElementChild);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+	const id = location.hash.substr(1);
+	
+	const newsInfo = getData(CONTENT_URL.replace('@id', id));
+
+	container.innerHTML = `
+		<h1>${newsInfo.title}</h1>
+		<div>
+			<a href="#">목록으로</a>
+		</div>
+	`;
+}
+
+function router() {
+	const routePath = location.hash;
+	
+	if (routePath == '') {
+		newsFeed();
+	} else {
+		newsDetail();
+	}
+}
+
+window.addEventListener('hashchange', router);
+
+router();
